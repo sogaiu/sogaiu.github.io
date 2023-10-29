20931faed9e40256b8fbe8cdd8333a44b26b30eb
sogaiu <983021772@users.noreply.github.com>
2023-10-29 16:38:28 +0900

    Add info on generating and use dark css

--- test/build.sh
+++ test/build.sh
@@ -1,3 +1,15 @@
 #! /bin/sh
 
+# how to make the style.css file:
+#
+# pygmentize -f html -S gruvbox-dark > ~/src/sogaiu.github.io/test/style.css
+
+# how to make the boot.janet.hl file:
+#
+# pygmentize -P style=gruvbox-dark \
+#             -f html \
+#             -O full,debug_token_types \
+#             -o  ~/src/sogaiu.github.io/test/boot.janet.hl \
+#             ~/src/janet/src/boot/boot.janet
+
 cat header boot.janet.hl footer > boot.janet.html
